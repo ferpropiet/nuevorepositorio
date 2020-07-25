a7b1601bb1e86c5578823f06cd6a7a51357be193
Fernando Carrasco <34533248+velocifer@users.noreply.github.com>
2020-07-24 23:53:38 -0300

    Add files via upload

---
+++ trabajo numero 8/javascript.js
@@ -0,0 +1,27 @@
+
+  function mostrar(caja) {
+  	var nombre = document.getElementById(caja)
+  	nombre.style.display='block'
+  }
+
+  function juego() {
+  	var comida = ['mariscos','humitas','pescado frito','mariscal']
+  	var nro = prompt('ingresa un numero entre 0 y 4')
+  	var codigo = prompt('ingresa el menu del dia : humitas,mariscos,pescado frito ,mariscal')
+  	var ganador = document.queryselector('#ganador_juego')
+  	 }
+
+ 
+  	if(codigo==comida[nro]) {
+  		alert('¡Ganaste!')
+  		
+  		ganador.style.display=block
+  } 
+
+  else {
+  	alert('perdiste...')
+
+  	ganador.style.display='none'
+  }
+
+
